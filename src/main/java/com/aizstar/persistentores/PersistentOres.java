4eb2e407c53d38b3832f24e1a95728166b712ecd
Sam <samstargazergustafsson@gmail.com>
2024-01-28 02:21:57 +0100

    Mod Init

---
+++ src/main/java/com/aizstar/persistentores/PersistentOres.java
@@ -0,0 +1,23 @@
+package com.aizstar.persistentores;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+
+@Mod(PersistentOres.MOD_ID)
+public class PersistentOres
+{
+    public static final String MOD_ID = "persistent_ores";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public PersistentOres()
+    {
+
+    }
+
+    public void func()
+    {
+
+    }
+}
